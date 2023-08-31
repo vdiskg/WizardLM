c35b826d8724b7c0d87e99bd1e5295e151c8828e
vdisk <vdisk@foxmail.com>
2023-08-31 20:02:42 +0800

    feat: max_split_size_mb

--- WizardCoder/src/eval_torchrun.sh
+++ WizardCoder/src/eval_torchrun.sh
@@ -11,6 +11,8 @@ gpu_num=4
 start_index=0
 end_index=164
 
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
+
 torchrun --nproc_per_node=${gpu_num} humaneval_gen.py --model ${model} --prompt_template ${prompt_template} --world_size ${gpu_num} \
       --start_index ${start_index} --end_index ${end_index} --temperature ${temp} \
       --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path}/tasks --greedy_decode
