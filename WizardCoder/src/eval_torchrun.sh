bd80260396f70dd0255d44226be8bddfaaca0150
vdisk <vdisk@foxmail.com>
2023-09-14 20:44:01 +0800

    Revert "feat: world_size"
    This reverts commit b9aadb21

--- WizardCoder/src/eval_torchrun.sh
+++ WizardCoder/src/eval_torchrun.sh
@@ -6,7 +6,7 @@ mkdir -vp ${output_path}/tasks
 echo 'Output path: '$output_path
 echo 'Model to eval: '$model
 
-gpu_num=4
+gpu_num=2
 
 start_index=0
 end_index=164
@@ -14,6 +14,6 @@ end_index=164
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
 
-torchrun --nproc_per_node=${gpu_num} humaneval_gen.py --model ${model} --prompt_template ${prompt_template} --world_size ${gpu_num} \
+torchrun --nproc_per_node=${gpu_num} humaneval_gen.py --model ${model} --prompt_template ${prompt_template} \
       --start_index ${start_index} --end_index ${end_index} --temperature ${temp} \
       --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path}/tasks --greedy_decode
